8a26f0abf0e5d800f6b1c49e41cd6e71d99466ab
Alex <ajobe15@gmail.com>
2018-09-21 21:06:09 -0500

    Add grid dictionary and pathfinder class

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour {
 
-    [SerializeField][Range(1f, 20f)] float gridSize = 10f;
+    Waypoint waypoint;
 
-    TextMesh textMesh;
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
 
-    void Update () {
+    void Update ()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
 
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x * gridSize, 
+            0,
+            waypoint.GetGridPos().y * gridSize
+        );
+    }
 
-        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
-
-        textMesh = GetComponentInChildren<TextMesh>();
-
-        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    private void UpdateLabel()
+    {
+        int gridSize = waypoint.GetGridSize();
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = 
+            waypoint.GetGridPos().x + 
+            "," +
+            waypoint.GetGridPos().y;
         textMesh.text = labelText;
         gameObject.name = labelText;
     }
---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour {
+
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+	// Use this for initialization
+	void Start () {
+        LoadBlocks();
+        ColorStartAndEnd();
+        ExploreNeighbors();
+	}
+
+    private void ExploreNeighbors()
+    {
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            try {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.Log("Overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
